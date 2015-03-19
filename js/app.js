73952cc377de78c4adb56a99bf227e4fb54128f4
FarazPatankar <farazpatankar@gmail.com>
2015-03-19 12:23:14 +0530

    Semicolons

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var Enemy = function(x,y) {
     this.x = x;
     this.y = y;
     this.speed = Math.floor((Math.random() * 200) + 100);
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -26,7 +26,7 @@ Enemy.prototype.update = function(dt) {
         this.x += this.speed * dt;
     }else{
         this.x = -2;
-    };
+    }
 
     //If the player comes within 50px of an enemy, reset the game and decrease player score unless the score is already 0
     if(player.x >= this.x - 50 && player.x <= this.x + 50){
@@ -35,16 +35,16 @@ Enemy.prototype.update = function(dt) {
             player.y = 400;
             if (score > 0) {
                 score--;
-            };
+            }
             document.getElementById('score').innerHTML = 'Score: '+score;
-        };
-    };
-}
+        }
+    }
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -60,7 +60,7 @@ var Player = function(){
 //Render function for the player
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 //Update function to handle player movements
 Player.prototype.update = function(){
@@ -85,13 +85,13 @@ Player.prototype.update = function(){
         player.y = 400;
         score++;
         document.getElementById('score').innerHTML = 'Score: '+score;
-    };
-}
+    }
+};
 
 //Input handler for player movement
 Player.prototype.handleInput = function(e){
     this.ctlKey = e;
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
